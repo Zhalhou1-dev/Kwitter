4fe2b2d93e402e80ecc4f54c1ca2b735a09fb488
Zhalhou1-dev <83404810+Zhalhou1-dev@users.noreply.github.com>
2021-12-04 15:33:13 +0530

    Update kwitter_room.js

--- kwitter_room.js
+++ kwitter_room.js
@@ -4,8 +4,21 @@ function log(){
 user=localStorage.getItem("user_name");
 function addRoom(){
     document.getElementById("add").value;
-    input=document.getElementById("
-    localStorage.setItem("store");
-    
+    input=document.getElementById("username").value;
+    localStorage.setItem("store",input);
     window.location("kwitter_page.html");
+}
+function getData() {firebase.database().ref("/").on('value',
+function(snapshot) {document.getElementById("output").innerHTML =
+"";snapshot.forEach(function(childSnapshot) {childKey = childSnapshot.key;
+Room_names = childKey;
+//Start code
+div="<div id='room'></div>"
+                                             
+//End code
+});});}
+getData();
+function redirecttoroomname(){
+localStorage.setItem("room_name",input);
+window.location("kwitter_page.html");
 }
